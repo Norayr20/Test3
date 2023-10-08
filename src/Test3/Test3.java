b372da2e779a36d0e870efba395ed9ae9f856978
Norayr <asoyannorayr99@gmail.com>
2023-10-08 18:52:24 +0300

    add

---
+++ src/Test3/Test3.java
@@ -0,0 +1,9 @@
+public class Test3 {
+    public static void main(String[] args) {
+            public int drawisMonitorsCounter(int monitors, int programmers) {
+                 monitors = 20;
+                 programmers = 20;
+                 drawisMonitorsCounter = monitors + programmers;
+            }
+        }
+    }
